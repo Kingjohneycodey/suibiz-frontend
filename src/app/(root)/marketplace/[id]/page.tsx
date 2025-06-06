286c3f154ab5647f2beabb74f663937d0d3558c5
Kariaki58 <kariakistephen809@gmail.com>
2025-06-06 09:00:31 +0100

    marketplace clean up

--- src/app/(root)/marketplace/[id]/page.tsx
+++ src/app/(root)/marketplace/[id]/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
 import { useParams, useRouter } from "next/navigation";
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
 import { Badge } from "@/components/ui/badge";
 import { Star, User } from "lucide-react";
 import Image from "next/image";
